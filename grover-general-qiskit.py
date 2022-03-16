0a6059f6f85092b226bac50d7dce100af748ebf2
JaimeAV <74024882+JaimeAlvaradoValiente@users.noreply.github.com>
2022-03-16 10:23:19 +0100

    Update grover-general-qiskit.py

--- grover-general-qiskit.py
+++ grover-general-qiskit.py
@@ -1,12 +1,25 @@
-from qiskit.aqua.algorithms import Grover
-from qiskit.aqua.components.oracles import LogicalExpressionOracle
+from qiskit import QuantumRegister, ClassicalRegister, QuantumCircuit
+from numpy import pi
 
+qreg_q = QuantumRegister(2, 'q')
+creg_c = ClassicalRegister(2, 'c')
+circuit = QuantumCircuit(qreg_q, creg_c)
 
-def get_circuit(**kwargs):
-    oracle_string = kwargs["formula"]   # input is logical expression, like '(A | B) & (A | ~B) & (~A | B)'
-
-    print(oracle_string)
-    oracle = LogicalExpressionOracle(oracle_string)
-    grover = Grover(oracle)
-    grover_circuit = grover.construct_circuit(measurement=True)
-    return grover_circuit
+circuit.reset(qreg_q[0])
+circuit.reset(qreg_q[1])
+circuit.h(qreg_q[1])
+circuit.h(qreg_q[0])
+circuit.x(qreg_q[1])
+circuit.x(qreg_q[0])
+circuit.cz(qreg_q[0], qreg_q[1])
+circuit.x(qreg_q[0])
+circuit.x(qreg_q[1])
+circuit.h(qreg_q[0])
+circuit.h(qreg_q[1])
+circuit.z(qreg_q[0])
+circuit.z(qreg_q[1])
+circuit.cz(qreg_q[0], qreg_q[1])
+circuit.h(qreg_q[0])
+circuit.h(qreg_q[1])
+circuit.measure(qreg_q[0], creg_c[0])
+circuit.measure(qreg_q[1], creg_c[1])
